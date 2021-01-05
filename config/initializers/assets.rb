56133585f533e32d34eeb9eb9ae7e4ce0c76d0a0
Masato516 <yaginen9@gmail.com>
2021-01-05 16:09:28 +0000

    cancel-bfcache

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( trix_uploads.js )
+Rails.application.config.assets.precompile += %w( cancel_bfcache.js trix_uploads.js )
