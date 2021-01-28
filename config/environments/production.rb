d91974ac532d05da3808f4fa7fea145788ca1b8d
Masato516 <yaginen9@gmail.com>
2021-01-28 18:39:43 +0000

    Change ses-API to ses-SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -69,6 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.dig(:aws, :host) }
   # SESのSMTPでメールを送信
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
