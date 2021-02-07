113e8d7939c7c31c2722820203a17ee3ae682084
Masato516 <yaginen9@gmail.com>
2021-02-07 17:31:03 +0000

    add remote-chrome on compose

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -11,9 +14,10 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+# Register new Driver for Capybara
 Capybara.register_driver :remote_chrome do |app|
-  url = "http://chrome:4444/wd/hub"
-  caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
+  # Specification of the desired and/or actual capabilities of the browser that the server is being asked to create.
+  chrome_capabilities = ::Selenium::WebDriver::Remote::Capabilities.chrome(
     "goog:chromeOptions" => {
       "args" => [
         "no-sandbox",
@@ -23,19 +27,19 @@ Capybara.register_driver :remote_chrome do |app|
       ]
     }
   )
-  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: ENV['SELENIUM_REMOTE_URL'], desired_capabilities: chrome_capabilities)
 end
 
 RSpec.configure do |config|
+  config.include Warden::Test::Helpers
 
   config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
-  config.before(:each, type: :system, js: true) do
     driven_by :remote_chrome
+    # Returns The IP address bound by default server
     Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+    # Returns the value of attribute server_port
     Capybara.server_port = 3000
+    # The default host to use when giving a relative URL to visit, must be a valid URL
     Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
   end
 
@@ -43,4 +47,5 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
 end
