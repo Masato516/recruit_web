3b8a0c76162266db55231140c14c54160b9d94f0
Masato516 <yaginen9@gmail.com>
2021-02-17 10:34:04 +0000

    add default admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Reward.find_or_create_by(id: 1, name: "報酬なし")
 Reward.find_or_create_by(id: 2, name: "謝金あり")
 Reward.find_or_create_by(id: 3, name: "その他")
 
-faculty_arry = %w(スポーツ健康科学部 食料マネジメント学部 理工学部 情報理工学部 生命科学部 薬学部 経済学部 経営学部 政策科学部 総合心理学部 グローバル教養学部 法学部 産業社会学部 国際関係学部 文学部 映像学部 外部)
+faculty_arry = %w(スポーツ健康科学部・研究科 食料マネジメント学部・研究科 理工学部・研究科 情報理工学部・研究科 生命科学部・研究科 薬学部・研究科 経済学部・研究科 経営学部・研究科 政策科学部・研究科 総合心理学部・研究科 グローバル教養学部・研究科 法学部・研究科 産業社会学部・研究科 国際関係学部・研究科 文学部・研究科 映像学部・研究科 外部)
 
 faculty_arry.map.with_index(1) {|faculty, index|
   Faculty.find_or_create_by(
@@ -15,3 +15,5 @@ faculty_arry.map.with_index(1) {|faculty, index|
     name: faculty
   )
 }
+
+User.find_or_create_by(id: 1, name: '管理者', email: Rails.application.credentials.dig(:admin_user, :email), password: Rails.application.credentials.dig(:admin_user, :password), confirmed_at: Time.now)
