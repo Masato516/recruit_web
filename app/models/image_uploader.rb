7daca9e559daa0acd563f70bce0170f44667eea4
Masato516 <yaginen9@gmail.com>
2021-02-17 10:41:30 +0000

    change file limiting size

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -2,6 +2,6 @@ class ImageUploader < Shrine
 	plugin :validation_helpers
 
   Attacher.validate do
-    validate_max_size 5 * 1024 * 1024, message: 'ファイルサイズは5MBまでです' 
+    validate_max_size 15 * 1024 * 1024, message: 'ファイルサイズは15MBまでです' 
   end
 end
