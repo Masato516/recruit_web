6a523f75d4be2f64bfd857a36872b71cc0092e1e
Masato516 <yaginen9@gmail.com>
2021-02-19 02:40:04 +0000

    add Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,7 +60,7 @@ namespace :deploy do
         on roles(:db) do |host|
             with rails_env: fetch(:rails_env) do
                 within current_path do
-                    execute :bundle, :exec, rake, 'db:create'
+                    execute :bundle, :exec, :rake, 'db:create'
                 end
             end
         end
