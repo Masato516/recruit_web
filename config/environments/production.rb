a091dc285ff99ea1b73a6da8e1bb6ab36c38a5a8
Masato516 <yaginen9@gmail.com>
2021-02-25 05:22:33 +0900

    add https to confirmation url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'www.recruit-rits.net' }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'www.recruit-rits.net' }
   # SESのSMTPでメールを送信
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
