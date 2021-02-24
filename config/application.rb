e5fb31005a0c24a5711bd1e8c61f6ca78d8feb29
Masato516 <yaginen9@gmail.com>
2021-02-24 09:56:01 +0900

    change to JST timezone

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module RecruitWeb
     config.i18n.default_locale = :ja
     # タイムゾーンの変更
     config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
