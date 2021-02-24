c675fed0b27a73d8e001d5dd26bde97730acc20a
Masato516 <yaginen9@gmail.com>
2021-02-24 09:46:24 +0900

    change to JST timezone

--- config/initializers/paranoia.rb
+++ config/initializers/paranoia.rb
@@ -1,4 +1,4 @@
 # ユーザー退会後の論理削除・一意制約を両立させるための処理
 # 非削除レコードはdeleted_at = '0000-01-01 00:00:00'
 # 削除済みレコードはdeleted_at != '0000-01-01 00:00:00'
-Paranoia.default_sentinel_value = DateTime.new(0)
+Paranoia.default_sentinel_value = Time.new(0)
