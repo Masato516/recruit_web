7c9340103f1cae521240495e48282cf8ab3f9cb5
Masato516 <yaginen9@gmail.com>
2021-03-03 13:40:52 +0900

    add tweet select box

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,17 +1,15 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :valid_user!, only: [:edit, :update, :destroy]
-  before_action :twitter_client, only: [:create]
+  before_action :valid_user?, only: [:edit, :update, :destroy]
+  before_action :twitter_client, only: [:create, :update]
 
   # GET /boards
-  # GET /boards.json
   def index
     @pagy, @boards = pagy(Board.all.order(id: :DESC))
   end
 
   # GET /boards/1
-  # GET /boards/1.json
   def show
   end
 
@@ -25,11 +23,14 @@ class BoardsController < ApplicationController
   end
 
   # POST /boards
-  # POST /boards.json
   def create
+    check_box = params[:board][:check]
     @board = Board.new(board_params)
-
-    if @board.save && @board.reward.name == "謝金なし"
+    
+    byebug
+    if check_box == "0"
+      redirect_to @board, notice: '募集要項を作成しました'
+    elsif @board.save && @board.reward.name == "謝金なし"
       @client.update("#{@board.title} #{board_url(@board.id)}")
       redirect_to @board, notice: '募集要項を作成しました'
     elsif @board.save
@@ -41,34 +42,38 @@ class BoardsController < ApplicationController
   end
 
   # PATCH/PUT /boards/1
-  # PATCH/PUT /boards/1.json
   def update
-      if @board.update(board_params)
-        redirect_to @board, notice: '募集要項を更新しました'
-      else
-        render :edit
-      end
+    check_box = params[:board][:check]
+
+    if @board.update(board_params) && check_box == "0"
+      redirect_to @board, notice: '募集要項を更新しました'
+    elsif @board.update(board_params) && @board.reward.name == "謝金なし"
+      @client.update("#{@board.title} #{board_url(@board.id)}")
+      redirect_to @board, notice: '募集要項を更新しました'
+    elsif @board.update(board_params)
+      @client.update("#{@board.title} #{board_url(@board.id)}\n #謝金あり")
+      redirect_to @board, notice: '募集要項を更新しました'
+    else
+      render :edit
+    end
   end
 
   # DELETE /boards/1
-  # DELETE /boards/1.json
   def destroy
     @board.destroy
       redirect_to boards_url, notice: '募集要項を削除しました'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_board
       @board = Board.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def board_params
       params.require(:board).permit(:title, :abstract, :detail, :campus_name_id, :laboratory, :start_day, :finish_day, :place, :reward_id, :reward_content, :number, :charge, :contact, :endline).merge(user_id: current_user.id)
     end
 
-    def valid_user!
+    def valid_user?
       board = Board.find(params[:id])
       unless current_user.id == board.user_id 
         flash[:danger] = '作成ユーザーでないと編集できません'
