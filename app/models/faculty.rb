36bb868eaca4c89e681485e1b885067e7ef35a96
Masato516 <yaginen9@gmail.com>
2021-07-11 20:48:12 +0900

    学部・研究科のActiveHash化

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,12 +1,21 @@
-# == Schema Information
-#
-# Table name: faculties
-#
-#  id         :bigint           not null, primary key
-#  name       :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-class Faculty < ApplicationRecord
-  has_many :users
+class Faculty < ActiveHash::Base
+  self.data = [
+    {id: 1,  name: 'スポーツ健康科学部・研究科'},
+    {id: 2,  name: '食料マネジメント学部・研究科'},
+    {id: 3,  name: '理工学部・研究科'},
+    {id: 4,  name: '情報理工学部・研究科'},
+    {id: 5,  name: '生命科学部・研究科'},
+    {id: 6,  name: '薬学部・研究科'},
+    {id: 7,  name: '経済学部・研究科'},
+    {id: 8,  name: '経営学部・研究科'},
+    {id: 9,  name: '政策科学部・研究科'},
+    {id: 10, name: '総合心理学部・研究科'},
+    {id: 11, name: 'グローバル教養学部・研究科'},
+    {id: 12, name: '法学部・研究科'},
+    {id: 13, name: '産業社会学部・研究科'},
+    {id: 14, name: '国際関係学部・研究科'},
+    {id: 15, name: '文学部・研究科'},
+    {id: 16, name: '映像学部・研究科'},
+    {id: 17, name: 'その他'}
+  ]
 end
