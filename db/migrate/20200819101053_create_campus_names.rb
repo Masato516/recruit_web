578f5b5b21972af2579fdfabe13d1a4cdd291c42
Masato516 <yaginen9@gmail.com>
2021-07-12 22:24:37 +0900

    ActiveHashの導入

--- db/migrate/20200819101053_create_campus_names.rb
+++
@@ -1,9 +0,0 @@
-class CreateCampusNames < ActiveRecord::Migration[5.2]
-  def change
-    create_table :campus_names do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20200819101220_create_rewards.rb
+++
@@ -1,9 +0,0 @@
-class CreateRewards < ActiveRecord::Migration[5.2]
-  def change
-    create_table :rewards do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
